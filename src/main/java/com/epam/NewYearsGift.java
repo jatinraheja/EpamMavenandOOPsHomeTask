b56d6b819bd1cdea2f546cd3f49921396518fa2d
Jatin Raheja <jatinraheja1999@gmail.com>
2020-02-02 20:33:26 +0530

    Another commit

--- src/main/java/com/epam/NewYearsGift.java
+++ src/main/java/com/epam/NewYearsGift.java
@@ -10,7 +10,7 @@ public class NewYearsGift {
         int n1 = sc.nextInt();
         System.out.println("Enter total number of candies to add in gift");
         int n2 = sc.nextInt();
-        System.out.println("The chocolates that are available are: Snikker , Perk, Dairy Milk, Five Star ");
+        System.out.println("The chocolates that are available are: Snikker,Perk, Dairy Milk, Five Star ");
         Sweet chocolates[] = new Chocolate[n1];
         Sweet candy[] = new Candy[n2];
         double totalweight=0;
@@ -27,7 +27,7 @@ public class NewYearsGift {
             chocolateweight[i] = chocolates[i].weight*quantity;
             totalweight+=chocolateweight[i];
          }
-
+        System.out.println("The candiess that are available are: Eclairs,Alpenliebe,Coconut candy,Mango Bite");
         for(int i=0;i<n2;i++)
         {
             System.out.println("Enter candy "+(i+1)+" name to add in gift");
@@ -50,10 +50,10 @@ public class NewYearsGift {
         {
             System.out.print(candy[i].name+" ");
         }
-
+        System.out.println();
         System.out.println("Total weight of the Gift is "+totalweight+"g");
 
-        System.out.println("Press 1 to sort the chocolates by it total weight");
+        System.out.println("Press 1 to sort the chocolates by it total weight or press 0 not sort");
         int choice = sc.nextInt();
         switch (choice)
         {
@@ -70,12 +70,35 @@ public class NewYearsGift {
                         }
                     }
                 }
-                System.out.println("The chocolates in the gift after sorting by the totalweight are");
+                System.out.println("The chocolates in the gift after sorting are");
                 for(int i=0;i<n1;i++)
                 {
                     System.out.print(chocolates[i].name+", ");
                 }
+                System.out.println();
         }
 
+        System.out.println("Enter 1 for searching a candy in the Gift");
+        int ch = sc.nextInt();
+        switch(ch)
+        {
+            case 1:
+                System.out.println("Enter the candy name to search in the gift");
+                sc.nextLine();
+                String cname = sc.nextLine();
+                for(int i=0;i<candy.length;i++)
+                {
+                    if(candy[i].name.equalsIgnoreCase(cname))
+                    {
+                        System.out.println("Candy "+cname+" is present in the gift");
+                        break;
+                    }
+                    else
+                    {
+                        System.out.println("Candy "+cname+" is not present in the Gift List");
+                        break;
+                    }
+                }
+        }
     }
 }
